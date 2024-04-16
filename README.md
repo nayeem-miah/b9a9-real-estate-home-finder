b92d968e5611f9546602f175a5604ab5e2b0e39f
nayeem-miah <nayeem5113a@gmail.com>
2024-04-16 14:21:14 +0600

    added-animate

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
-
 ## npm package
 - firebase
 - prop-types
 - sweetalert2
 - react-icons
+- tailwind
 ## Implement of 3 packages
 - react-hook-form
 - animate.css
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import img2 from '../../assets/imgad.jpg';
 import img3 from '../../assets/2382897.jpg'
 const Header = () => {
   return (
-    <div className="carousel w-full my-10">
+    <div className="carousel w-full my-10 animate__animated animate__slideInUp">
   <div id="slide1" className="carousel-item relative w-full">
     <img src={image} className="w-full h-screen" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
