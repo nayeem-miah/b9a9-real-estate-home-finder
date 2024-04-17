2a57f28a0cd0bd9b4bf124b1144eb4d4bd64409e
nayeem-miah <nayeem5113a@gmail.com>
2024-04-17 11:34:22 +0600

    added-live-link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-gray-800 shadow-lg">
+    <div className="navbar bg-gray-800 shadow-lg ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
